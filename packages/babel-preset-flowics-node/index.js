1173d3e6a0f448c5f67aa5eef842823b582dffc5
Mariano A. Cortesi <mariano@flowics.com>
2016-09-13 12:22:47 -0300

    Fix invalid require (v2.0.1)

--- packages/babel-preset-flowics-node/index.js
+++ packages/babel-preset-flowics-node/index.js
@@ -4,7 +4,7 @@ module.exports = {
 
     // ES7+ Support
     require('babel-plugin-transform-exponentiation-operator'),
-    require('babel-plugin-transform-async-to-generator'),
+    require('babel-plugin-transform-async-to-module-method'),
     require('babel-plugin-transform-object-rest-spread'),
     require('babel-plugin-transform-class-properties')
   ],
