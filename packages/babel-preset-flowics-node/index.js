ed08a88fbdb7b1676ecf70e27edda0fdc8083f27
Mariano A. Cortesi <mariano@flowics.com>
2016-10-05 19:13:32 -0300

    [babel-node] Fix async/await support & add flow support

--- packages/babel-preset-flowics-node/index.js
+++ packages/babel-preset-flowics-node/index.js
@@ -2,9 +2,16 @@ module.exports = {
   plugins: [
     require('babel-plugin-transform-es2015-modules-commonjs'),
 
+    // Flow Support
+    require('babel-plugin-transform-flow-strip-types'),
+
     // ES7+ Support
     require('babel-plugin-transform-exponentiation-operator'),
-    require('babel-plugin-transform-async-to-module-method'),
+    require('babel-plugin-syntax-async-functions'),
+    [require('babel-plugin-transform-async-to-module-method'), {
+      "module": "bluebird",
+      "method": "coroutine"
+    }],
     require('babel-plugin-transform-object-rest-spread'),
     require('babel-plugin-transform-class-properties')
   ],
