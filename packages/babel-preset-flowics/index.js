a8e7ff8496854f53d1f9cf33a809c8708f6cdb8b
Mariano A. Cortesi <mariano@flowics.com>
2016-10-06 14:22:47 -0300

    [babel-preset] 2.0.0 Fix async/await & add Flow support

--- packages/babel-preset-flowics/index.js
+++ packages/babel-preset-flowics/index.js
@@ -4,10 +4,19 @@ module.exports = {
     require('babel-preset-react'),
   ],
   plugins: [
+    // Flow Support
+    require('babel-plugin-transform-flow-strip-types'),
+
     // sandbox polyfills, use babel runtine on apps
     [require('babel-plugin-transform-runtime'), {"polyfill": true, "regenerator": true }],
+
+    // ES7+ Support
     require('babel-plugin-transform-exponentiation-operator'),
-    require('babel-plugin-transform-async-to-generator'),
+    require('babel-plugin-syntax-async-functions'),
+    [require('babel-plugin-transform-async-to-module-method'), {
+      "module": "bluebird",
+      "method": "coroutine"
+    }],
     require('babel-plugin-transform-object-rest-spread'),
     require('babel-plugin-transform-class-properties'),
   ],
