b0d31185f54754dd1b33faf552dcc00ef25d4e7b
Manuel Gambino <manuel@flowics.com>
2017-02-07 14:29:54 -0300

    [babel-flowics-preset] Use modules false for tree shaking. Update presets

--- packages/babel-preset-flowics/index.js
+++ packages/babel-preset-flowics/index.js
@@ -1,23 +1,28 @@
 module.exports = {
   presets: [
-    require('babel-preset-es2015'),
-    require('babel-preset-react'),
+    ['es2015', {
+      modules: false
+    }],
+    'react'
   ],
   plugins: [
     // Flow Support
-    require('babel-plugin-transform-flow-strip-types'),
+    'transform-flow-strip-types',
 
     // sandbox polyfills, use babel runtine on apps
-    [require('babel-plugin-transform-runtime'), {"polyfill": true, "regenerator": true }],
+    ['transform-runtime', {
+      polyfill: true,
+      regenerator: true
+    }],
 
     // ES7+ Support
-    require('babel-plugin-transform-exponentiation-operator'),
-    require('babel-plugin-syntax-async-functions'),
-    [require('babel-plugin-transform-async-to-module-method'), {
+    'transform-exponentiation-operator',
+    'syntax-async-functions',
+    ['transform-async-to-module-method', {
       "module": "bluebird",
       "method": "coroutine"
     }],
-    require('babel-plugin-transform-object-rest-spread'),
-    require('babel-plugin-transform-class-properties'),
+    'transform-object-rest-spread',
+    'transform-class-properties',
   ],
 };
