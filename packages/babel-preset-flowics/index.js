c2d04addcbf3c3cc847149f7883c2390591d4ec3
Manuel Gambino <manuel@flowics.com>
2017-02-07 15:28:04 -0300

    [babel-flowics-preset] using imports. v3.0.2

--- packages/babel-preset-flowics/index.js
+++ packages/babel-preset-flowics/index.js
@@ -1,28 +1,38 @@
+import es2015 from 'babel-preset-es2015';
+import react from 'babel-preset-react';
+import flowStripTypes from 'babel-plugin-transform-flow-strip-types';
+import runtime from 'babel-plugin-transform-runtime';
+import exponentiationOperator from 'babel-plugin-transform-exponentiation-operator';
+import asyncFunctions from 'babel-plugin-syntax-async-functions';
+import asyncToModule from 'babel-plugin-transform-async-to-module-method';
+import objectRestSpread from 'babel-plugin-transform-object-rest-spread';
+import classProperties from 'babel-plugin-transform-class-properties';
+
 module.exports = {
   presets: [
-    ['es2015', {
+    [es2015, {
       modules: false
     }],
-    'react'
+    react
   ],
   plugins: [
     // Flow Support
-    'transform-flow-strip-types',
+    flowStripTypes,
 
     // sandbox polyfills, use babel runtine on apps
-    ['transform-runtime', {
+    [runtime, {
       polyfill: true,
       regenerator: true
     }],
 
     // ES7+ Support
-    'transform-exponentiation-operator',
-    'syntax-async-functions',
-    ['transform-async-to-module-method', {
-      "module": "bluebird",
-      "method": "coroutine"
+    exponentiationOperator,
+    asyncFunctions,
+    [asyncToModule, {
+      module: "bluebird",
+      method: "coroutine"
     }],
-    'transform-object-rest-spread',
-    'transform-class-properties',
+    objectRestSpread,
+    classProperties,
   ],
 };
