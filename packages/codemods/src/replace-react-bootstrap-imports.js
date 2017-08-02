19cba4c30d15016661ee070613b2b4b3dc6348f6
Mariano A. Cortesi <mariano@flowics.com>
2017-08-02 17:11:11 -0300

    codemod to replace react-bootstrap imports

--- packages/codemods/src/replace-react-bootstrap-imports.js
+++ packages/codemods/src/replace-react-bootstrap-imports.js
@@ -16,49 +16,78 @@ const modulesThatExportDefault = (modules) =>
 const moduleDefinitions = (module, exportedSymbols) =>
   exportedSymbols.map(symbol => ({ type: 'keyword', oldName: symbol, module }));
 
-const DEFINITIONS = [
-  { type: 'default', oldName: 'withConfirmation', module: 'Confirmation'  },
-]
-  .concat(modulesThatExportDefault([
-    'Alert',
-    'DynamicSizeText',
-    'EditableText',
-    'EmptyMessage',
-    'FAIcon',
-    'InfoTooltip',
-    'Input',
-    'Loader',
-    'MediaTiles',
-    'Mention',
-    'MentionModalWithControls',
-    'NetworkError',
-    'PageHeader',
-    'Panel',
-    'Select',
-    'SourceIndicator',
-    'StatusIndicator',
-    'Switch',
-    'TextArea',
-    'Tooltip',
-    'VideoLoader',
-    'TwitterIntent',
-    'KBLink'
-  ]))  
-  .concat(moduleDefinitions('KBLink', ['KBDropdown']))
-  .concat(moduleDefinitions('AppLayout', ['AppLayout', 'AppNav', 'AppContent']))
-  .concat(moduleDefinitions('Breadcrumb', ['Breadcrumb', 'BreadcrumbItem']))
-  .concat(moduleDefinitions('Button', ['Button', 'ButtonGroup', 'ButtonCounter']))
-  .concat(moduleDefinitions('CheckboxRadio', ['Checkbox', 'Radio']))
-  .concat(moduleDefinitions('Dropdown', ['DropdownToggle', 'Dropdown', 'DropdownItem', 'GenericDropdownItem', 'DropdownDivider', 'DropdownGroup']))
-  .concat(moduleDefinitions('Grid', ['Container', 'Row', 'Col']))
-  .concat(moduleDefinitions('Icon', ['Icon', 'UIIcon']))
-  .concat(moduleDefinitions('ImageLoader', ['ImageFetcher', 'ImageLoader', 'DivImageLoader']))
-  .concat(moduleDefinitions('ListItem', ['ListItem', 'ListItemActions']))
-  .concat(moduleDefinitions('Modal', ['Modal', 'ModalHeader', 'ModalBody', 'ModalFooter']))
-  .concat(moduleDefinitions('Popover', ['PopoverToggle', 'Popover', 'PopoverTitle', 'PopoverBody']))
-  .concat(moduleDefinitions('Topbar', ['Topbar', 'ContextProvider']))
-  .concat(moduleDefinitions('BrowserInfo', ['getUserAgent', 'isTouch']))
-  .concat(moduleDefinitions('BrowserPopup', ['centeredPopup']))
+const DEFINITIONS = modulesThatExportDefault([
+  'Accordion',
+  'Alert',
+  'Badge',
+  'Breadcrumb',
+  'BreadcrumbItem',
+  'Button',
+  'ButtonGroup',
+  'ButtonToolbar',
+  'Carousel',
+  'CarouselItem',
+  'Checkbox',
+  'Clearfix',
+  'CloseButton',
+  'ControlLabel',
+  'Col',
+  'Collapse',
+  'Dropdown',
+  'DropdownButton',
+  'Fade',
+  'Form',
+  'FormControl',
+  'FormGroup',
+  'Glyphicon',
+  'Grid',
+  'HelpBlock',
+  'Image',
+  'InputGroup',
+  'Jumbotron',
+  'Label',
+  'ListGroup',
+  'ListGroupItem',
+  'Media',
+  'MenuItem',
+  'Modal',
+  'ModalBody',
+  'ModalFooter',
+  'ModalHeader',
+  'ModalTitle',
+  'Nav',
+  'Navbar',
+  'NavbarBrand',
+  'NavDropdown',
+  'NavItem',
+  'Overlay',
+  'OverlayTrigger',
+  'PageHeader',
+  'PageItem',
+  'Pager',
+  'Pagination',
+  'PaginationButton',
+  'Panel',
+  'PanelGroup',
+  'Popover',
+  'ProgressBar',
+  'Radio',
+  'ResponsiveEmbed',
+  'Row',
+  'SafeAnchor',
+  'SplitButton',
+  'Tab',
+  'TabContainer',
+  'TabContent',
+  'Table',
+  'TabPane',
+  'Tabs',
+  'Thumbnail',
+  'ToggleButton',
+  'ToggleButtonGroup',
+  'Tooltip',
+  'Well'
+  ]);
 
 export const parser = 'flow';
 
@@ -129,15 +158,14 @@ export default (fileInfo, api) => {
     }
   }
 
-
-  const importsRewriter = new ImportsRewriter(DEFINITIONS, (_, moduleName) => `flui/lib/${moduleName}`);
+  const importsRewriter = new ImportsRewriter(DEFINITIONS, (_, moduleName) => `react-bootstrap/lib/${moduleName}`);
 
   const root = j(fileInfo.source);
   return root
     .find(j.ImportDeclaration, {
       source: {
         type: 'Literal',
-        value: 'flui',
+        value: 'react-bootstrap',
       },
     })
     .forEach(path => importsRewriter.rewrite(path))
